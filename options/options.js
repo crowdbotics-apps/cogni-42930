9a043e5bc2d1c7d8a17a709d2af0700dba4fd580
Dorukan Ucar <dorukanucar@gmail.com>
2023-08-18 14:09:52 +0200

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,10 +1,11 @@
 export const globalOptions = {
-  name: "cogni-42930",
-  url: "https://cogni-42930.botics.co",
-  api: "https://cogni-42930.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
+  "name": "cogni-42930",
+  "url": "https://cogni-42930.botics.co",
+  "api": "https://cogni-42930.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "Untitled5"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
--- store/cogniAPI/api.js
+++ store/cogniAPI/api.js
@@ -1,50 +1,72 @@
-import axios from "axios"
+import axios from "axios";
 const cogniAPI = axios.create({
   baseURL: "https://cogni-42930.botics.co",
-  headers: { Accept: "application/json", "Content-Type": "application/json" }
-})
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json"
+  }
+});
+
 function api_docs_schema_retrieve(payload) {
-  return cogniAPI.get(`/api-docs/schema/`, { params: { lang: payload.lang } })
+  return cogniAPI.get(`/api-docs/schema/`, {
+    params: {
+      lang: payload.lang
+    }
+  });
 }
+
 function api_v1_login_create(payload) {
-  return cogniAPI.post(`/api/v1/login/`, payload.data)
+  return cogniAPI.post(`/api/v1/login/`, payload.data);
 }
+
 function api_v1_signup_create(payload) {
-  return cogniAPI.post(`/api/v1/signup/`, payload.data)
+  return cogniAPI.post(`/api/v1/signup/`, payload.data);
 }
+
 function rest_auth_login_create(payload) {
-  return cogniAPI.post(`/rest-auth/login/`, payload.data)
+  return cogniAPI.post(`/rest-auth/login/`, payload.data);
 }
+
 function rest_auth_logout_retrieve(payload) {
-  return cogniAPI.get(`/rest-auth/logout/`)
+  return cogniAPI.get(`/rest-auth/logout/`);
 }
+
 function rest_auth_logout_create(payload) {
-  return cogniAPI.post(`/rest-auth/logout/`)
+  return cogniAPI.post(`/rest-auth/logout/`);
 }
+
 function rest_auth_password_change_create(payload) {
-  return cogniAPI.post(`/rest-auth/password/change/`, payload.data)
+  return cogniAPI.post(`/rest-auth/password/change/`, payload.data);
 }
+
 function rest_auth_password_reset_create(payload) {
-  return cogniAPI.post(`/rest-auth/password/reset/`, payload.data)
+  return cogniAPI.post(`/rest-auth/password/reset/`, payload.data);
 }
+
 function rest_auth_password_reset_confirm_create(payload) {
-  return cogniAPI.post(`/rest-auth/password/reset/confirm/`, payload.data)
+  return cogniAPI.post(`/rest-auth/password/reset/confirm/`, payload.data);
 }
+
 function rest_auth_registration_create(payload) {
-  return cogniAPI.post(`/rest-auth/registration/`, payload.data)
+  return cogniAPI.post(`/rest-auth/registration/`, payload.data);
 }
+
 function rest_auth_registration_verify_email_create(payload) {
-  return cogniAPI.post(`/rest-auth/registration/verify-email/`, payload.data)
+  return cogniAPI.post(`/rest-auth/registration/verify-email/`, payload.data);
 }
+
 function rest_auth_user_retrieve(payload) {
-  return cogniAPI.get(`/rest-auth/user/`)
+  return cogniAPI.get(`/rest-auth/user/`);
 }
+
 function rest_auth_user_update(payload) {
-  return cogniAPI.put(`/rest-auth/user/`, payload.data)
+  return cogniAPI.put(`/rest-auth/user/`, payload.data);
 }
+
 function rest_auth_user_partial_update(payload) {
-  return cogniAPI.patch(`/rest-auth/user/`, payload.data)
+  return cogniAPI.patch(`/rest-auth/user/`, payload.data);
 }
+
 export const apiService = {
   api_docs_schema_retrieve,
   api_v1_login_create,
@@ -60,4 +82,4 @@ export const apiService = {
   rest_auth_user_retrieve,
   rest_auth_user_update,
   rest_auth_user_partial_update
-}
+};
