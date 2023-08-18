5d6b20c5c47bd5f1d773823a7f89e8bbeaf750e3
Crowdbotics <team@crowdbotics.com>
2023-08-18 12:00:40 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "cogni-42930",
+  url: "https://cogni-42930.botics.co",
+  api: "https://cogni-42930.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
